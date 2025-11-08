3c70bda4a248d4e1548dd9a0b11482b3aa88822f
luiz-dias <luizdiasnti@gmail.com>
2025-11-07 22:11:59 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  // Validation Pipe
+  // Validation Pipe teste
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
